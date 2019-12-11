6caba00ea0fce11a677cc4ca27617a00064a43d6
konradotto <gusottko@student.gu.se>
2019-12-11 03:35:24 +0100

    Remove unneccessary print from __main__.py

--- src/__main__.py
+++ src/__main__.py
@@ -14,5 +14,5 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--sleep', help='set the time (in seconds) the emitter sleeps between publishing '
                                               'two consecutive requests[float]', type=float)
     parser.add_argument('-o', '--offset', help='set the uncertainty of coordinate seeds in meters [float]', type=float)
-    print(parser.parse_args())
+
     overpass_handler.run(sys.argv[1:])
